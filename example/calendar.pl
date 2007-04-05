ea05b4081161c9c4b07c2965479204de926d74a4
Christopher J. Madsen <perl@cjmweb.net>
2007-04-05 03:22:30 +0000

    Import of CJM/PostScript-Calendar-0.02 from CPAN.
    gitpan-cpan-distribution: PostScript-Calendar
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CJM/PostScript-Calendar-0.02.tar.gz
    gitpan-cpan-author:       CJM
    gitpan-cpan-maturity:     released

--- example/calendar.pl
+++ example/calendar.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl
 #---------------------------------------------------------------------
-# $Id: calendar.pl 1726 2007-03-25 23:14:01Z cjm $
+# $Id: calendar.pl 1732 2007-04-04 23:27:18Z cjm $
 # Copyright 2007 Christopher J. Madsen
 #
 # This program is free software; you can redistribute it and/or modify
@@ -70,10 +70,10 @@ calendar.pl - Generate a monthly calendar in PostScript
 
 =head1 SYNOPSIS
 
-Usage:  calendar.pl [options] YEAR MONTH [PARAMETER VALUE ...]
-  -o, --output=NAME  Save calendar to NAME.ps (default YEAR-MONTH)
-  -?, --help         Display this usage information and exit
-      --version      Display version number and exit
+ Usage:  calendar.pl [options] YEAR MONTH [PARAMETER VALUE ...]
+   -o, --output=NAME  Save calendar to NAME.ps (default YEAR-MONTH)
+   -?, --help         Display this usage information and exit
+       --version      Display version number and exit
 
 =head1 DESCRIPTION
 
@@ -104,7 +104,7 @@ at L<http://rt.cpan.org/Public/Bug/Report.html?Queue=PostScript-Calendar>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2007, Christopher J. Madsen. All rights reserved.
+Copyright 2007 Christopher J. Madsen. All rights reserved.
 
 This module is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself. See L<perlartistic>.
